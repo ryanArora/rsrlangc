418ece960b78944b1af79dd78edfbafcc1a12a5b
Ryan Arora <arora.ryan@gmail.com>
2023-12-07 10:57:15 -0500

    add arguments parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+mod arch;
+mod output_format;
+use arch::Arch;
+use output_format::OutputFormat;
+
+#[derive(clap::Parser, Debug)]
+struct Args {
+    input_file: std::path::PathBuf,
+    #[arg(short, default_value = "a.bin")]
+    output_file: std::path::PathBuf,
+    #[arg(long, value_enum, default_value_t = Arch::X86_64)]
+    arch: Arch,
+    #[arg(long, value_enum, default_value_t = OutputFormat::Binary)]
+    output_format: OutputFormat,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _args = <Args as clap::Parser>::parse();
 }
